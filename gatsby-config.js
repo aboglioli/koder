15fa05fc947e36f6901d2dec2018f6635e82611b
Alan Boglioli <alan.boglioli@gmail.com>
2019-02-28 19:24:25 -0300

    disable trackingId from analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-82052179-2',
+        // trackingId: 'UA-82052179-2',
       },
     },
     'gatsby-plugin-feed',
