541b4148b2ade0be216f321f7ef97eccb976a4c8
Alan Boglioli <alan.boglioli@gmail.com>
2019-03-01 21:11:45 -0300

    change who-am-i page

--- src/pages/who-am-i.js
+++ src/pages/who-am-i.js
@@ -8,9 +8,14 @@ const WhoAmI = () => (
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
+      flexDirection: 'column',
+      textAlign: 'center',
+      padding: '1rem',
     }}
   >
     <h1>I don not know!</h1>
+    <h2>I would also like to know.</h2>
+    <h3>While I discover it, I will continue to add bugs to my code.</h3>
   </main>
 );
 
